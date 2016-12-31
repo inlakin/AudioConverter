56532deef80f505b6bed16d12800d3846200e110
Robin Désir <desir@ece.fr>
2016-12-31 05:00:11 -0500

    Adding some comments

--- AudioConvertor.py
+++ AudioConvertor.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python
 
+# TODO
+# 
+#  * Recuperer les métadonnées des pistes
+#  * Pouvoir proposer un format d'entrée et de sortie en ligne de commande
+#  * Progress Bar pour la conversion
+#  (GUI)
+#  
+
 import os
 import re
 import sys
@@ -11,8 +19,9 @@ RELATIVE_DIR_PATTERN = r"\/([^\/\\]+)$"
 FILE_SUFFIXE         = ".mp3"
 FILE_PATTERN         = r"([^\/\\]+)\.flac$"
 
-file_converted = 0
+file_converted       = 0
 
+# For printing purposes : instead of printing the absolute path we are in, we print the relative one. 
 def relative_dir_name(dirname):
     res = re.search(RELATIVE_DIR_PATTERN, str(dirname))
     if res:
@@ -22,7 +31,7 @@ def relative_dir_name(dirname):
         print "[-] relative_dir_name(dirname) function :  No match - Exiting .."
         sys.exit(0)
 
-
+# Function that return a new name for the file we are converting (adding the correct extension)
 def new_file_name(filename):
     new = re.search(FILE_PATTERN, str(filename))
     if new:
@@ -32,7 +41,8 @@ def new_file_name(filename):
         print "[-] new_file_name(filename) function :  No match - Exiting .."
         sys.exit(0)
 
-
+# Convert filename from the old_dir destination to the new_dir destination 
+# MUST BE IMPROVED WITH the new_extension argument which will induce the writing of other functions (or not.. see pydub)
 def convert(old_dir, new_dir,filename):
 
     print "\t[*] Processing ..."     
@@ -40,7 +50,8 @@ def convert(old_dir, new_dir,filename):
     sound.export(new_dir + "/" + new_file_name(filename), format="mp3", bitrate="192k")
     print "\t[*] Done."
 
- 
+
+# Main loop
 for dirname, dirnames, filenames in os.walk(os.getcwd()):
 
     rel_dirname = relative_dir_name(dirname)
